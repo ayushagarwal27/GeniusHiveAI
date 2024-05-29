395a652b1efec4814a37abf6a64121771726cb1f
Ayush <ayushagarwal.dev@gmail.com>
2024-05-30 00:03:31 +0530

    add UI fixes

--- src/components/ExpertCard.tsx
+++ src/components/ExpertCard.tsx
@@ -8,12 +8,12 @@ interface ExpertCardProps {
   expertInfo: ExpertsDataType;
 }
 
-const ExpertCard: FC<ExpertCardProps> = ({ expertInfo }) => {
+const ExpertCard: FC<ExpertCardProps> = ({ expertInfo, marginBottom }) => {
   const router = useRouter();
   return (
     <div
       className={
-        " group bg-transparent text-white flex flex-col gap-4 p-4 text-center cursor-pointer border-2 border-yellow-100 rounded-lg hover:-translate-y-2 transition hover:shadow-2xl   backdrop-filter backdrop-blur-lg bg-opacity-40"
+        "group bg-transparent text-white flex flex-col gap-4 p-4 text-center cursor-pointer border-2 border-yellow-100 rounded-lg hover:-translate-y-2 transition hover:shadow-2xl   backdrop-filter backdrop-blur-lg bg-opacity-40"
       }
       onClick={() => router.push(`/expert/${expertInfo.name}`)}
     >
@@ -21,7 +21,7 @@ const ExpertCard: FC<ExpertCardProps> = ({ expertInfo }) => {
         src={expertInfo.content.imgUrl}
         alt={parseExpertName(expertInfo.name)}
         className={
-          "inline-block  group-hover:shadow-amber-100  rounded-lg mx-auto shadow-2xl  h-[300px] w-full object-cover"
+          "inline-block  group-hover:shadow-amber-100  rounded-lg mx-auto shadow-2xl  h-[280px] w-full object-cover"
         }
       />
       <div className={"italic"}>{parseExpertName(expertInfo.name)}</div>
--- src/components/ExpertsLayoutContainer.tsx
+++ src/components/ExpertsLayoutContainer.tsx
@@ -6,7 +6,7 @@ const ExpertsLayoutContainer = () => {
   return (
     <div
       className={
-        "m-4 mt-1 p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-[1000px]  xl:max-w-[1200px] mx-auto"
+        "m-4 mt-1 mb-[60px] p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-[1000px]  xl:max-w-[1200px] mx-auto"
       }
     >
       {expertsData.map((expert, index) => (
