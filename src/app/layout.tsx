08f7357c28fe69e419ee5ec4c857393e8c74746b
Ayush <ayushagarwal.dev@gmail.com>
2024-05-31 14:07:58 +0530

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import { AuroraBackground } from "@/components/ui/aurora-background";
 import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body className={inter.className + " relative"}>
         <Navbar />
         {children}
